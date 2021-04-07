10dca0452aae290358a8d1a537bab377774dec27
Marcoshsc <marcoshscunha@hotmail.com>
2021-04-07 02:41:04 -0300

    remove old code

--- antcolony.py
+++ antcolony.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from random import randint, random
 
 def fitness(solution, pairsWeight):
@@ -89,69 +88,3 @@ def antColony(nTrips, nVehicles, nWorkers, durations, depreciation, benefit):
     print(f'Best fitness: {best}')
 
   print(pheromony)
-=======
-import json
-import random
-from funcoes import objective
-
-data = dict
-
-with open('data.json', 'r') as jsFile:
-    data = json.loads(jsFile.read())
-
-beta = data['beta']
-alpha = data['alpha']
-p = data['p']
-n_max = data['n_max']
-vehicles = []
-
-for trip in data['trips']:
-    if trip['vehicle'] not in vehicles:
-        vehicles.append(trip['vehicle'])
-
-vehicleTrips = [[] for v in vehicles]
-
-trips = [t for t in data['trips']]
-
-for trip in data['trips']:
-    vehicleTrips[trip['vehicle'] - 1].append(trip)
-
-workers = [w for w in data['workers']]
-
-
-#print(trips)
-#print(vehicles)
-#print(workers)
-
-n_iter = 0
-n_ants = len(trips)
-while n_iter < n_max:
-    solutions = []
-    for i in range(n_ants):
-        solution = [] # solucao da formiga
-        vehicleStatus = [False for v in vehicles]
-        workerStatus = [False for w in workers]
-        attendedRoutes = [False for t in trips]
-
-        for w in workers:
-            vehicle = getFreeVehicle(vehicleStatus)
-            if vehicle is None: # Sem veiculo livre
-                continue
-
-            trip = vehicleTrips[vehicle][random.randint(0, len(vehicleTrips[vehicle]))]
-            
-            if attendedRoutes[trip['sequencial']]:   
-                continue
-                
-            attendedRoutes[trip] = True
-            workerStatus[trip] = True
-            solution.append(trip)
-            print(solution)
-
-            
-
-            
-        
-
-    n_iter += 1
->>>>>>> 9c33fe65696da9ee6a40dfd3a8b56e877ed5304e
